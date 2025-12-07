fee2b49f13a661b19341ce5e62decbbfeec99169
tijina123 <tijinamariamtitus@gmail.com>
2025-12-07 13:34:15 +0530

    Updated header, replaced resume PDF

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -24,26 +24,28 @@ const Header = () => {
          initial = {{y: -30, opacity: 0}}
         whileInView = {{y: 0, opacity: 1}}
         transition = {{duration: 0.8, delay: 0.5}}
-        className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Full Stack Developer Based in Pathanamthitta</motion.h1>
+        className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>Full Stack Developer Based in Doha, Qatar</motion.h1>
         <motion.p 
          initial = {{ opacity: 0}}
         whileInView = {{opacity: 1}}
         transition = {{duration: 0.6, delay: 0.7}}
         className='max-w-2xl mx-auto font-Ovo'>I am a Full Stack Developer from Pathanamthitta,Kerala with 1 year experience in company Cloud7 Branding Studio</motion.p>
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-            <motion.a 
-             initial = {{y: 30, opacity: 0}}
-        whileInView = {{y: 0, opacity: 1}}
-        transition = {{duration: 0.6, delay: 1}}
-            href='#contact'
-            className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 '>
-                Contact me  <Image src={assets.right_arrow_white} alt='' className=' w-4'/></motion.a>
-
-             <motion.a 
+            <motion.a
+  initial={{ y: 30, opacity: 0 }}
+  whileInView={{ y: 0, opacity: 1 }}
+  transition={{ duration: 0.6, delay: 1 }}
+  href="tel:+97470418700"   // ← this makes it call the number
+  className="px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2"
+>
+  Contact me
+  <Image src={assets.right_arrow_white} alt="" className="w-4" />
+</motion.a>
+            <motion.a
               initial = {{y: 30, opacity: 0}}
         whileInView = {{y: 0, opacity: 1}}
         transition = {{duration: 0.6, delay: 1.2}}
-             href='/Tijina-resume.pdf' download 
+             href='/TIJINA MARIAM TITUS (cv).pdf' download 
              className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
                 my resume  <Image src={assets.download_icon} alt='' className=' w-4'/></motion.a>
         </div>
